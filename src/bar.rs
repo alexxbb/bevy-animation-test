d68d081ad8aa38bd330c96881fb084454490e59b
Aleksei Rusev <hou.alexx@gmail.com>
2024-01-02 22:39:57 -0800

    Testing basic idea

---
+++ src/bar.rs
@@ -0,0 +1,75 @@
+use bevy::prelude::*;
+use bevy::render::mesh::VertexAttributeValues;
+
+#[derive(Component)]
+pub(crate) struct Bar;
+
+#[derive(Bundle)]
+pub struct BarBundle {
+    mesh: PbrBundle,
+    marker: Bar,
+}
+
+impl BarBundle {
+    fn new(origin: Vec3, mesh: Handle<Mesh>, material: Handle<StandardMaterial>) -> Self {
+        BarBundle {
+            mesh: PbrBundle {
+                mesh,
+                material,
+                transform: Transform::from_translation(origin),
+                ..default()
+            },
+            marker: Bar,
+        }
+    }
+    fn mesh() -> impl Into<Mesh> {
+        shape::Box::new(0.5, 4.0, 0.5)
+    }
+}
+
+pub fn create_bar_grid(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let material = materials.add(Color::rgb(0.9, 0.1, 0.1).into());
+    const SIZE: i32 = 10;
+    for i in -SIZE..SIZE {
+        for j in -SIZE..SIZE {
+            let origin = Vec3::new(i as f32, 0., j as f32);
+            let mesh = meshes.add(BarBundle::mesh().into());
+            let bar = BarBundle::new(origin, mesh, material.clone());
+            commands.spawn(bar);
+        }
+    }
+}
+
+pub fn rotate_bars(mut query: Query<&mut Transform, With<Bar>>, time: Res<Time>) {
+    let elapsed = time.elapsed_seconds();
+    for (i, mut tr) in query.iter_mut().enumerate() {
+        let rot = (elapsed * 1.0 + i as f32).sin() * 0.1;
+        tr.rotate_local_y(rot);
+    }
+}
+
+pub fn deform_bar(
+    mut meshes: ResMut<Assets<Mesh>>,
+    query: Query<&Handle<Mesh>, With<Bar>>,
+    time: Res<Time>,
+) {
+    let start = std::time::Instant::now();
+    let time = time.elapsed_seconds();
+    for (mesh_n, handle) in query.iter().enumerate() {
+        if let Some(mut mesh) = meshes.get_mut(handle) {
+            let VertexAttributeValues::Float32x3(ref mut data) = mesh
+                .attribute_mut(Mesh::ATTRIBUTE_POSITION)
+                .expect("Mesh attribute not found")
+            else {
+                return;
+            };
+            for point in data.iter_mut() {
+                point[1] += ((mesh_n as f32 + time) * 10.).sin() * 0.05;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,46 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-mod _main;
+mod bar;
+use bar::*;
 
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::pbr::DirectionalLightShadowMap;
 use bevy::prelude::*;
 use bevy::render::mesh::VertexAttributeValues;
 use bevy::window::close_on_esc;
 
-#[derive(Component)]
-struct Bar;
-
-#[derive(Component)]
-struct Ground;
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(DirectionalLightShadowMap { size: 2048 })
-        .add_systems(Startup, setup)
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_plugins(LogDiagnosticsPlugin {
+            debug: false,
+            filter: Some(vec![FrameTimeDiagnosticsPlugin::FPS]),
+            ..default()
+        })
+        .insert_resource(DirectionalLightShadowMap { size: 1024 })
+        .add_systems(Startup, (default_scene, create_bar_grid))
+        .add_systems(Update, (rotate_bars, deform_bar))
         .add_systems(Update, close_on_esc)
-        .add_systems(Update, rotate)
-        .add_systems(Update, update_bar)
         .run();
 }
 
-fn update_bar(
-    mut meshes: ResMut<Assets<Mesh>>,
-    query: Query<&Handle<Mesh>, With<Bar>>,
-    time: Res<Time>,
-) {
-    let time = time.elapsed_seconds();
-    for (mesh_n, handle) in query.iter().enumerate() {
-        if let Some(mut mesh) = meshes.get_mut(handle) {
-            let VertexAttributeValues::Float32x3(ref mut data) = mesh
-                .attribute_mut(Mesh::ATTRIBUTE_POSITION)
-                .expect("Mesh attribute not found")
-            else {
-                return;
-            };
-            for point in data.iter_mut() {
-                point[1] += (mesh_n as f32 + time * 10.0).sin() * 0.25;
-            }
-        }
-    }
-}
-
-fn rotate(mut query: Query<&mut Transform, With<Bar>>, time: Res<Time>) {
-    let elapsed = time.elapsed_seconds();
-    for (i, mut tr) in query.iter_mut().enumerate() {
-        let rot = (elapsed * 4.0 + i as f32).sin() * 0.1;
-        tr.rotate_local_y(rot);
-    }
-}
-
-fn setup(
+fn default_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut ambient: ResMut<AmbientLight>,
 ) {
-    // plane
     ambient.brightness = 0.3;
+
     commands.spawn((
         PbrBundle {
             mesh: meshes.add(shape::Plane::from_size(20.).into()),
             material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
             ..default()
         },
-        Ground,
+        Name::new("grid"),
     ));
 
     // light
@@ -88,14 +60,4 @@ fn setup(
         transform,
         ..default()
     });
-
-    for i in 0..4 {
-        let b = PbrBundle {
-            mesh: meshes.add(shape::Box::new(2.0, 4.0, 2.0).into()),
-            material: materials.add(Color::rgb(0.9, 0.1, 0.1).into()),
-            transform: Transform::from_xyz(-8.0 + (i * 5) as f32, 4.0, 0.0),
-            ..default()
-        };
-        commands.spawn((b, Bar));
-    }
 }
